9dad5a3205735814599a8be21f7a3a642360a04c
gilKag2 <35850376+gilKag2@users.noreply.github.com>
2019-03-31 22:31:15 +0300

    need repairs
    cd and ls sometimes doesnt work, need to check jobs control

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 #include <wait.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pwd.h>
+
 #define  INPUT_SIZE 512
-#define  NUM_OF_ARGS 512
 #define  MAX_JOBS 512
 #define BACKGROUND 0
 #define FOREGROUND 1
 
 struct Job {
     char command[INPUT_SIZE];
-    char * args[INPUT_SIZE];
     pid_t  pid;
     char mode;
     int index;
@@ -23,28 +23,123 @@ struct ShellInfo {
     char cwd[INPUT_SIZE];
     char pwd[INPUT_SIZE];
     int numOfJobs;
-} shellInfo;
+};
+
+struct ShellInfo shellInfo;
 
 
 void addJobToList(struct Job* job) {
+    shellInfo.jobs[shellInfo.numOfJobs] = NULL;
     job->index = shellInfo.numOfJobs;
     shellInfo.jobs[shellInfo.numOfJobs] = job;
     shellInfo.numOfJobs++;
 }
 
-int execProcess(struct Job* job) {
+
+
+void updatePwd(){
+    strcpy(shellInfo.pwd, shellInfo.cwd);
+}
+
+void updateCwd() {
+    getcwd(shellInfo.cwd, sizeof(shellInfo.cwd));
+}
+
+void activateCdCommand(char** args) {
+    printf("%d\n", getpid());
+    if (args[1] == NULL || strcmp(args[1], "~") == 0) {
+        struct passwd *pwd = getpwuid(getuid());
+        if (chdir(pwd->pw_dir) != 0) {
+            fprintf(stderr, "Error in system call");
+            return;
+        }
+        updatePwd();
+        updateCwd();
+
+    } else if (strcmp(args[1], "-")== 0) {
+        if (strcmp(shellInfo.pwd, shellInfo.cwd) != 0) {
+            printf("%s\n", shellInfo.pwd);
+            if (chdir(shellInfo.pwd) != 0)
+                fprintf(stderr, "Error in system call");
+            // update the parent directory to be the current.
+            updatePwd();
+            // update the current directory.
+            updateCwd();
+        }
+    }
+    else{
+        char *str;
+        strcpy(str, args[1]);
+        if (chdir(args[1]) != 0)
+            fprintf(stderr, "Error in system call");
+    }
+}
+
+
+
+void freeShell() {
+    int i;
+    for (i = 0; i < shellInfo.numOfJobs; i++) {
+        shellInfo.jobs[i] = NULL;
+    }
+
+}
+
+void printJobs() {
+    int i;
+    for (i = 0; i < shellInfo.numOfJobs; i++) {
+        printf("%s\n", shellInfo.jobs[i]->command);
+    }
+}
+
+
+void removeJob(int idx)
+{
+    shellInfo.jobs[idx] = NULL;
+    for (int i = idx; i < shellInfo.numOfJobs - 1; i++) {
+        shellInfo.jobs[i] = shellInfo.jobs[i+1];
+        shellInfo.jobs[i]->index--;
+        shellInfo.jobs[i+1] = NULL;
+    }
+    shellInfo.numOfJobs--;
+
+}
+void checkStatus() {
+    int status;
+
+    for (int i = 0; i < shellInfo.numOfJobs; i++) {
+        if (waitpid(shellInfo.jobs[i]->pid, &status, WNOHANG) != 0) {
+            removeJob(i);
+        }
+    }
+}
+int execute(struct Job* job, char** args) {
+   
+    if (strcmp(args[0], "cd") == 0) {
+        activateCdCommand(args);
+        return 1;
+    }
+    else if (strcmp(args[0], "exit") == 0) {
+        freeShell();
+        exit(0);
+    } else if(strcmp(args[0], "jobs") == 0) {
+        checkStatus();
+        printJobs();
+        return 1;
+    }
+
     pid_t  pid;
     int status;
     if ((pid = fork()) == 0) {
         printf("%d\n", getpid());
-        if (execvp(job.args[0], job.args) == -1) {
-          fprintf(stderr, "Error in system call");
+        if (execvp(args[0], args) == -1) {
+            fprintf(stderr, "Error in system call");
         }
 
     } else if (pid != -1){
         if (job->mode == BACKGROUND) {
             job->pid = pid;
-            printf("%d\n", pid);
+            printf("%d\n", getpid());
             // add the job to the jobs list.
             addJobToList(job);
             return pid;
@@ -60,121 +155,72 @@ int execProcess(struct Job* job) {
 }
 
 
-void updatePwd(){
-    strcpy(shellInfo.pwd, shellInfo.cwd);
-}
-
-void updateCwd() {
-    getcwd(shellInfo.cwd, sizeof(shellInfo.cwd));
-}
-
-void activateCdCommand(char** args) {
-    printf("%d\n", getpid());
-    if (args[1] == NULL || strcmp(args[1], "~") == 0) {
-        if (chdir(getenv("HOME")) != 0)
-            fprintf(stderr, "Error in system call");
-    } else if (strcmp(args[1], "-")== 0) {
-        if (shellInfo.pwd[0] != '\0') {
-            printf("%s\n", shellInfo.pwd);
-
-            if (chdir(shellInfo.pwd) != 0)
-                fprintf(stderr, "Error in system call");
-            // update the parent directory to be the current.
-            updatePwd();
-            // update the current directory.
-            updateCwd();
-        }
-        else
-            fprintf(stderr, "Error in system call");
-    }
-    else{
-        if (chdir(args[1]) != 0)
-            fprintf(stderr, "Error in system call");
-    }
-}
-
-void freeShell() {
-    free(shellInfo.cwd);
-    free(shellInfo.pwd);
-    int i;
-    for (i = 0; i < shellInfo.numOfJobs; i++) {
-        shellInfo.jobs[i] = NULL;
-    }
-}
-
-void printJobs() {
-    int i;
-    for (i = 0; i < shellInfo.numOfJobs; i++) {
-        printf("%s\n", shellInfo.jobs[i]->command);
-    }
-}
-int execute(char ** args) {
-    if (args[0] == NULL) {
-        // no command
-        return 1;
-    }
-    if (strcmp(args[0], "cd") == 0) {
-        activateCdCommand(args);
-        return 1;
-    }
-    else if (strcmp(args[0], "exit") == 0) {
-
-        exit(0);
-    } else if(strcmp(args[0], "jobs") == 0) {
-        printJobs();
-    }
-
-    return execProcess();
-}
-
-
 void readLine(char* input) {
     fgets(input, INPUT_SIZE, stdin);
     strtok(input, "\n");
 }
 
-void parseToArgs(char* line, char** args) {
+char** parseToArgs(char* line) {
+    char** args = (char**)malloc(INPUT_SIZE * sizeof(char*));
+    // in case of alloc error.
+    if (!args) {
+        fprintf(stderr, "Error in system call");
+    }
     int counter = 0;
     char * token = strtok(line, " ");
+
     while(token != NULL) {
-        args[counter++] = token;
+       args[counter] = token;
+       counter++;
         token = strtok(NULL, " ");
     }
+    return args;
 }
 
 void displayPrompt() {
-    printf("< ");
+    printf("%s > ", shellInfo.cwd);
 }
 
 void init() {
-    int i;
-    for (i = 0; i < MAX_JOBS; i++) {
-        shellInfo.jobs[i] = NULL;
-    }
     shellInfo.numOfJobs = 0;
+    updateCwd();
+    updatePwd();
 }
 
-void startShell() {
+
+void activateShell() {
     int status;
     // one array for the input line, and the other is the parsed array.
-    char line[INPUT_SIZE], *args[NUM_OF_ARGS];
 
     do{
+
+        char line[INPUT_SIZE];
+        char ** args;
+
         displayPrompt();
         readLine(line);
-        int isBackground = 0;
+        char mode = FOREGROUND;
+        if (strlen(line) == 0) continue;
+        
         if (line[strlen(line) - 1] == '&') {
-            isBackground = 1;
+            mode = BACKGROUND;
             line[strlen(line) - 1] = '\0';
         }
-        parseToArgs(line, args);
+        args = parseToArgs(line);
+        struct Job * job = (struct Job*) malloc(sizeof(struct Job));
+        strcpy(job->command , line);
+        job->pid = -1;
+        job->mode = mode;
 
-        free(line);
+        status = execute(job, args);
         free(args);
+        sleep(1);
     } while (status);
+
 }
 
 int main() {
-    startShell();
+    init();
+    activateShell();
     return 0;
 }
